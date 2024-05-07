1d329dc56b3d3f827eb63962225be4a49b94b90c
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-05-07 03:04:57 -0700

    in HomePage fecth data from coingecko.com api

--- src/components/modules/templates/HomePage.jsx
+++ src/components/modules/templates/HomePage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getCoinList } from '../../../services/CryptoApi';
 
 function HomePage() {
+
+    const[coins , setCoins] = useState([]);
+
+    useEffect(()=>{
+        const getData = async()=>{
+            const res = fetch(getCoinList());
+            const json = res.json();
+            setCoins(json);
+        }
+        getData();
+    },[]);
   return (
     <div>
-      HomePage
     
     </div>
   )
