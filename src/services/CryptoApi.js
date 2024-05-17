cb020b621a323970b23a7010ce8609a0314da453
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-05-17 07:55:41 -0700

    search Result is complete and this section now show the search result

--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -3,5 +3,6 @@ const API_KEY = "CG-E1Ebr36Koz9ZtRhAdWZhuh6z";
 
 function getCoinList(currency,page){
     return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&sparkline=false&locale=en&x_cg_demo_api_key=${API_KEY}`
-}
-export {getCoinList};
+};
+const searchCoins = (query) => `https://api.coingecko.com/api/v3/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
+export {getCoinList,searchCoins};
